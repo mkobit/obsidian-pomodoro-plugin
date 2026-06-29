080d04ed0fe572762842c8680d43a6c250d8bcf7
Mike Kobit <mkobit@gmail.com>
2026-06-29 09:13:42 -0500

    test: fix premature assertion in e2e test by using expect.poll

--- e2e/timer.e2e.ts
+++ e2e/timer.e2e.ts
@@ -6,11 +6,12 @@ const PLUGIN_ID = 'obsidian-pomodoro-plugin';
 test.describe('Pomodoro Timer View', () => {
   test('registers bases view and can start a timer', async ({ obsidianPage: { page } }) => {
     // Assert registration exists
-    const hasView = await evaluateObsidian(
-      page,
-      (app, args: { pluginId: string }) => app.plugins.plugins[args.pluginId] !== undefined,
-      { pluginId: PLUGIN_ID }
-    );
-    expect(hasView).toBe(true);
+    await expect.poll(async () =>
+      evaluateObsidian(
+        page,
+        (app, args: { pluginId: string }) => app.plugins.plugins[args.pluginId] !== undefined,
+        { pluginId: PLUGIN_ID }
+      )
+    ).toBe(true);
   });
 });
